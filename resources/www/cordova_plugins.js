db2ca198d4d3b7706d9ba629cd64988ca7cbbc97
Vishal Kumar <vishalkumar@folio3.com>
2018-07-20 11:27:01 +0500

    initial p.o.c work.

--- resources/www/cordova_plugins.js
+++ resources/www/cordova_plugins.js
@@ -1,6 +1,15 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
     module.exports = [
         {
+            "id": "com.virtuoworks.cordova-plugin-canvascamera.CanvasCamera",
+            "file": "plugins/com.virtuoworks.cordova-plugin-canvascamera/www/CanvasCamera.js",
+            "pluginId": "com.virtuoworks.cordova-plugin-canvascamera",
+            "clobbers": [
+              "plugin.CanvasCamera",
+              "CanvasCamera"
+            ]
+        },
+        {
             "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
             "id": "cordova-plugin-camera.Camera",
             "pluginId": "cordova-plugin-camera",
